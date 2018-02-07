852758c29b89365ee5cc7dc24576a923ab09f7ed
Zoltan Makadi <zltnmakadi@gmail.com>
2018-02-07 15:49:24 +0100

    feat(re_pl): dashboard jsx component created

---
+++ src/js/dashboard.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import styles from '../scss/dashboard.scss';
+import React, { Component } from 'react';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard-container">
+      </div>
+    );
+  }
+}
