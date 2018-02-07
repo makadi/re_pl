1579dc97e5a2775fb5d5c06df189e218454bb8fa
Zoltan Makadi <zltnmakadi@gmail.com>
2018-02-07 15:47:16 +0100

    feat(re_pl): login component created

---
+++ src/js/login.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import styles from '../scss/login.scss';
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="login-container">
+      </div>
+    );
+  }
+}
