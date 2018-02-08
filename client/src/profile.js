b96a0146038eee3c3291db0514ea17b25a510da4
Zoltan Makadi <zltnmakadi@gmail.com>
2018-02-08 07:38:05 +0100

    feat(re_pl, profile): added example props for sitebuild

--- client/src/profile.js
+++ client/src/profile.js
@@ -4,6 +4,15 @@ import { PageHeader } from 'react-bootstrap';
 export default class Profile extends Component {
   constructor(props) {
     super(props);
+    this.props = {
+      userExample: {
+        name: 'Thisis Yourname',
+        email: 'thisis-youremail@address.com',
+        birth: '1989',
+        info: 'Basic information from you and your activities..',
+        genre: 'Ambient, Deep'
+      }
+    }
   }
   
   render() {
