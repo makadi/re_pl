66e06576d8cd54d3d60a8287662a01530d41867b
Zoltan Makadi <zltnmakadi@gmail.com>
2018-02-08 03:34:40 +0100

    feat(re_pl): validator created for login form

--- server/routes/validator.js
+++ server/routes/validator.js
@@ -35,6 +35,44 @@ function validateSignupForm(payload) {
   };
 }
 
+function validateLoginForm(payload) {
+  const errors = {};
+  let isFormValid = true;
+  let message = '';
+
+  if (!payload || typeof payload.email !== 'string' || payload.email.trim().length === 0) {
+    isFormValid = false;
+    errors.email = 'Please provide your email address.';
+  }
+
+  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length === 0) {
+    isFormValid = false;
+    errors.password = 'Please provide your password.';
+  }
+
+  if (!isFormValid) {
+    message = 'Check the form for errors.';
+  }
+
+  return {
+    success: isFormValid,
+    message,
+    errors
+  };
+}
+
+router.post('/login', (req, res) => {
+  const validationResult = validateLoginForm(req.body);
+  if (!validationResult.success) {
+    return res.status(400).json({
+      success: false,
+      message: validationResult.message,
+      errors: validationResult.errors
+    });
+  }
+
+  return res.status(200).end();
+});
 
 router.post('/signup', (req, res) => {
   const validationResult = validateSignupForm(req.body);
@@ -48,3 +86,6 @@ router.post('/signup', (req, res) => {
 
   return res.status(200).end();
 });
+
+
+module.exports = router;
