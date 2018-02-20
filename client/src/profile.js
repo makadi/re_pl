abfe4b34943d915d3aa68cb1b7613810be7cda01
Zoltan Makadi <zltnmakadi@gmail.com>
2018-02-20 17:43:12 +0100

    feat(re_pl): profile page modified

--- client/src/profile.js
+++ client/src/profile.js
@@ -7,44 +7,42 @@ import styles from './scss/profile.scss';
 export default class Profile extends Component {
   constructor(props) {
     super(props);
-    this.props = {
-      userExample: {
-        name: 'Thisis Yourname',
-        email: 'thisis-youremail@address.com',
-        birth: '1989',
-        info: 'Basic information from you and your activities..',
-        genre: 'Ambient, Deep'
-      }
-    }
+    this.props = props;
   }
-  
+
   render() {
+    const userExample = ({
+      fullname: 'Zoltan Makadi',
+      email: {
+        primary: 'primary-email@domain.com',
+        secondary: 'secondary-email@domain.com'
+      },
+      birth: '1989',
+      genre: 'Ambient, Deep',
+      info: 'Basic information about me and my activities..'
+    })
+    
     return (
       <div className="profile-container">
-        <PageHeader>
-          Thisis Yourname <small>About</small>
-        </PageHeader>
-        <Panel>
-          <Panel.Heading>Panel heading without a title</Panel.Heading>
-          <Panel.Body>Panel content</Panel.Body>
-        </Panel>
-        <Panel>
-          <Panel.Heading>Panel heading without a title</Panel.Heading>
-          <Panel.Body>Panel content</Panel.Body>
-        </Panel>
-        <Panel>
-          <Panel.Heading>Panel heading without a title</Panel.Heading>
-          <Panel.Body>Panel content</Panel.Body>
-        </Panel>
-        <Panel>
-          <Panel.Heading>Panel heading without a title</Panel.Heading>
-          <Panel.Body>Panel content</Panel.Body>
-        </Panel>
-        <Panel>
-          <Panel.Heading>Panel heading without a title</Panel.Heading>
-          <Panel.Body>Panel content</Panel.Body>
-        </Panel>
+        <div className="info-container">
+          <div className="page-header">Settings</div>
+          <div className="info-head">E-mail addresses</div>
+          <div className="info-row email">{userExample.email.primary} (Primary)</div>
+          <div className="info-row email">{userExample.email.secondary}</div>
+          <button className="add-email-button">Add an e-mail address</button>
+          <div className="info-head">Password</div>
+          <button className="add-email-button">Change password</button>
+          <div className="info-head">Basic Information</div>
+          <div className="info-row">Birth date Gender</div>
+          <div className="info-head">Favorite genres</div>
+          <div className="info-row">{userExample.genre}</div>
+          <div className="info-head">About you</div>
+          <div className="info-row">{userExample.info}</div>
+        </div>
+        <div className="profile-sidebar">
+        </div>
       </div>
     );
   }
 }
+
