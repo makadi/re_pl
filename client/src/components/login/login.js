ac5dcfa66d173b7f65973e395d6d2e53cb8b5b71
Zoltan Makadi <zltnmakadi@gmail.com>
2018-02-28 16:52:33 +0100

    feat(login): basic login page frame created with input fields

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -4,13 +4,44 @@ import styles from '../../styles/login.scss';
 
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.name = null;
+    this.password = null;
+    this.confirmPassword = null;
+    this.email = null;
+  }
   render() {
+    let inputs = (
+      <div className="login_inputs_container">
+        <input
+          className="login_name"
+          type="text"
+          placeholder="name"
+          ref={(name) => { this.name = name; }}
+        />
+        <input
+          className="login_name"
+          type="password"
+          placeholder="password"
+          ref={(password) => { this.password = password; }}
+        />
+      </div>
+    )
+
     return (
-      <Layout>
-        <div className="login-container">
-          <h1>LOGIN</h1>
+      <div className="login-container">
+        {inputs}
+        <div className="container__button">
+            <button
+              ref={(button) => { this.loginButton = button; }}
+              className="login_button"
+              onClick={this.handleLogin}
+            >
+              LOGIN
+            </button>
         </div>
-      </Layout>
+      </div>
     );
   }
 }
