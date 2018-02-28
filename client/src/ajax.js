3d370835edbb387eff078809a6b51fa094effa5b
Zoltan Makadi <zltnmakadi@gmail.com>
2018-02-28 15:07:17 +0100

    feat(collection, ajax): catch playlists data with ajax function from the endpoint

---
+++ client/src/ajax.js
@@ -0,0 +1,27 @@
+
+function mainAjax(data) {
+  return new Promise(function(resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(data.method, data.url);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText));
+      } else if (xhr.readyState === 4 && xhr.status !== 200) {
+        reject(xhr.responseText);
+      }
+    }
+    xhr.send(JSON.stringify(data.body));
+  });
+}
+
+function getPlaylists() {
+  let url = 'http://localhost:3000/playlists';
+  return mainAjax({
+    method: 'GET',
+    url: url,
+    body: ''
+  });
+}
+
+module.exports = {getPlaylists};
--- client/src/components/collection/collection.js
+++ client/src/components/collection/collection.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import styles from '../../styles/collection.scss';
 import Layout from '../layout';
+import { getPlaylistData, createPlaylistElements } from './getCollection';
+import { getPlaylists } from '../../ajax';
 
 
 export default class Collection extends Component {
+  constructor(props) {
+    super(props);
+    getPlaylistData();
+  }
+
   render() {
     return (
       <Layout>
         <div className="collection-container">
           <h1>COLLECTION</h1>
+          <ul className="playlists-container">Playlists</ul>
+          <ul className="tracklist-container">Tracklist</ul>
         </div>
       </Layout>
     );
